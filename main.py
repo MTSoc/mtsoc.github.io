86a4bc413b46133900cdf0e2f521b77b628060ca
Jack Finnis <jack.finnis@icloud.com>
2023-08-30 23:42:41 +0100

    Simplified file structure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# You can run this file to test the website locally
+
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_folder='', template_folder='')
 
 @app.route('/')
 def root():
