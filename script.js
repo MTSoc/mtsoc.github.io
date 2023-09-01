360f1052b148378155bcec7ef32df551017ee035
Jack Finnis <jack.finnis@icloud.com>
2023-09-02 00:08:36 +0100

    Google Analytics, nav bar animation and better player integration

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // Setup
 let logo = document.getElementById('logo')
 let nav_links = document.getElementById('nav_links')
+let nav_menu_img = document.getElementById('nav_menu_img')
+let intro = document.getElementById('intro')
 let player_cover = document.getElementById('player_cover')
 let down_button = document.getElementById('down_button')
 let mute_img = document.getElementById('mute_img')
 let unmute_img = document.getElementById('unmute_img')
-
-// On load
-mute_img.style.display = 'none'
 
 // Functions
 let player;
@@ -15,52 +14,84 @@ function onYouTubePlayerAPIReady() {
     player = new YT.Player('player', {
         videoId: 'KoUbSExui7g',
         playerVars: {
-            'autoplay': 1,
-            'loop': 1,
-            'controls': 0,
-            'playlist': 'KoUbSExui7g'
+            autoplay: 1,
+            controls: 0
         },
         events: {
-            'onReady': start_player
+            onReady: on_player_ready,
+            onStateChange: on_player_state_change
         }
     })
 }
 
-function start_player() {
+function on_player_ready(event) {
     player.mute()
     player.playVideo()
+}
 
-    // Hide MTSoc logo when singing starts
-    setTimeout(function() {
-        logo.style.opacity = 0
-        player_cover.style.opacity = 0
-    }, 11_000)
+function on_player_state_change(event) {  
+    let player_element = document.getElementById('player') 
+    console.log(event.data)
+    switch (event.data) {
+    case YT.PlayerState.PLAYING:
+        // Show player
+        player_element.style.opacity = 1
+        // Hide MTSoc logo just before the singing starts in the video
+        setTimeout(function() {
+            logo.style.opacity = 0
+            player_cover.style.opacity = 0
+        }, 11_000)
+        break
+    case YT.PlayerState.ENDED:
+        // Hide player
+        player_element.style.opacity = 0
+        // Show static logo
+        logo.style.opacity = 1
+        // Scroll down to next section when video finishes
+        if (document.body.scrollTop === 0) {          
+            scroll_to('about')
+        }
+        break
+    }
 }
 
 function toggle_mute() {
     if (player) {
         if (player.isMuted()) {
             player.unMute()
-            mute_img.style.display = 'block'
-            unmute_img.style.display = 'none'
+            mute_button_img.src = '/images/symbols/speaker.wave.3.fill.png'
         } else {
             player.mute()
-            mute_img.style.display = 'none'
-            unmute_img.style.display = 'block'
+            mute_button_img.src = '/images/symbols/speaker.slash.fill.png'
         }
     }
 }
 
-function toggle_nav_links() {
-    if (nav_links.classList.contains('hidden')) {
-        nav_links.classList.remove('hidden')
+function update_nav_menu_img() {
+    if (nav_links.classList.contains('selected')) {
+        nav_menu_img.src = '/images/symbols/xmark.png'
     } else {
-        nav_links.classList.add('hidden')
+        nav_menu_img.src = '/images/symbols/line.3.horizontal.png'
     }
+}
+
+function toggle_nav_links() {
+    nav_links.classList.add('transition')
+    nav_links.classList.toggle('selected')
+    update_nav_menu_img()
+    setTimeout(function() {
+        nav_links.classList.remove('transition')
+    }, 350)
 }
 
 function scroll_to(anchor) {
     let element = document.getElementById(anchor)
     element.scrollIntoView()
-    nav_links.classList.add('hidden')
+
+    nav_links.classList.add('transition')
+    nav_links.classList.add('selected')
+    toggle_nav_links()
 }
+
+// On load
+update_nav_menu_img()
